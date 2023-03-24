a7d02e48be7e2a1fb1c8ba8661fe1a7571ee6a09
Alvin Novian <alvinnovian883@gmail.com>
2023-03-24 21:44:29 +0700

    improve transformer to handle @Assets

--- transformer.ts
+++ transformer.ts
@@ -1,7 +1,7 @@
 import { API, FileInfo, ImportSpecifier } from "jscodeshift";
 import { ripgrep } from "./ripgrep";
 
-const test = true;
+const test = false;
 const ALIASES_TEST = ["@components", "@utils", "@types"];
 const ALIASES_REAL = [
   "@Styles",
@@ -19,7 +19,7 @@ const ALIASES_REAL = [
 
 const ALIASES = test ? ALIASES_TEST : ALIASES_REAL;
 
-export default function transformer(file: FileInfo, api: API) {
+const findComponentsImport = (file: FileInfo, api: API) => {
   const j = api.jscodeshift;
   const filePath = file.path;
 
@@ -28,54 +28,106 @@ export default function transformer(file: FileInfo, api: API) {
       j.ImportDeclaration,
       (node) =>
         typeof node.source.value === "string" &&
-        ALIASES.includes(node.source.value),
+        /\/components$/i.test(node.source.value),
+    )
+    .forEach((path) => {
+      console.log(filePath);
+    });
+};
+
+const normalizeContentType = (file: FileInfo, api: API) => {
+  const j = api.jscodeshift;
+  const filePath = file.path;
+
+  return j(file.source)
+    .find(
+      j.ImportDeclaration,
+      (node) =>
+        typeof node.source.value === "string" &&
+        node.source.value === "src/Constants/ContentType",
+    )
+    .forEach((path) => {
+      console.log(filePath);
+      if (typeof path.value.source.value === "string")
+        path.value.source.value = "src/GQL/generated/graphql-types";
+    })
+    .toSource();
+};
+
+export default function transformer(file: FileInfo, api: API) {
+  // return findComponentsImport(file, api);
+  // return normalizeContentType(file, api);
+
+  const j = api.jscodeshift;
+  const filePath = file.path;
+
+  return j(file.source)
+    .find(
+      j.ImportDeclaration,
+      (node) =>
+        typeof node.source.value === "string" &&
+        (ALIASES.includes(node.source.value) ||
+          node.source.value.includes("@Assets/")),
     )
     .forEach((path) => {
       const alias = path.get("source").value.value;
 
-      if (!path.value.specifiers.length || path.value.specifiers.length === 0)
+      if (alias.includes("@Assets/")) {
+        if (typeof path.value.source.value === "string")
+          path.value.source.value = path.value.source.value.replace(
+            "@Assets/",
+            "src/Assets/",
+          );
+      } else if (
+        !path.value.specifiers.length ||
+        path.value.specifiers.length === 0
+      ) {
         console.log(`${filePath} - ERROR`);
+      } else {
+        const modules = path.value.specifiers.filter(
+          (n) => n.type === "ImportSpecifier",
+        ) as ImportSpecifier[];
 
-      const modules = path.value.specifiers.filter(
-        (n) => n.type === "ImportSpecifier",
-      ) as ImportSpecifier[];
+        modules.forEach((m) => {
+          const searchResults = ripgrep(m.imported.name);
 
-      modules.forEach((m) => {
-        const searchResults = ripgrep(m.imported.name);
+          // With our codebase's codestyle, the resolved file path / the original code
+          // will highly likely be the one with lesser path depth
+          const resolvedFilePath = searchResults?.[0]?.replace(
+            /\.(tsx|ts)$/,
+            "",
+          );
 
-        // With our codebase's codestyle, the resolved file path / the original code
-        // will highly likely be the one with lesser path depth
-        const resolvedFilePath = searchResults?.[0];
+          const isLocalized = m.local.name !== m.imported.name;
+          const importName = isLocalized
+            ? `${m.imported.name}(${m.local.name})`
+            : m.local.name;
 
-        const isLocalized = m.local.name !== m.imported.name;
-        const importName = isLocalized
-          ? `${m.imported.name}(${m.local.name})`
-          : m.local.name;
+          console.log(
+            `${filePath} -> ${alias} -> ${importName} -> ${
+              resolvedFilePath ?? "NONE"
+            }`,
+          );
 
-        console.log(
-          `${filePath} -> ${alias} -> ${importName} -> ${
-            resolvedFilePath ?? "NONE"
-          }`,
+          j(path).insertAfter(
+            j.importDeclaration(
+              [
+                j.importSpecifier(
+                  j.identifier(m.imported.name),
+                  j.identifier(m.local.name),
+                ),
+              ],
+              j.literal(resolvedFilePath),
+            ),
+          );
+        });
+
+        path.value.specifiers = path.value.specifiers.filter(
+          (n) => n.type !== "ImportSpecifier",
         );
 
-        j(path).insertAfter(
-          j.importDeclaration(
-            [
-              j.importSpecifier(
-                j.identifier(m.imported.name),
-                j.identifier(m.local.name),
-              ),
-            ],
-            j.literal(resolvedFilePath),
-          ),
-        );
-      });
-
-      path.value.specifiers = path.value.specifiers.filter(
-        (n) => n.type !== "ImportSpecifier",
-      );
-
-      if (path.value.specifiers.length === 0) j(path).remove();
+        if (path.value.specifiers.length === 0) j(path).remove();
+      }
     })
     .toSource();
 }
